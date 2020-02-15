7f875379ac6ab20e83e179d924298a261d4ab15f
surajkarki66 <suraj.karki500@gmail.com>
2020-02-15 13:12:41 +0545

    Finished bubble sort and Selection sort

---
+++ Python/Sort/SortingVisualizations/app.py
@@ -0,0 +1,89 @@
+import time
+import sys
+import random
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+from sorts import Sort
+
+
+if __name__ == "__main__":
+    method_msg = """
+
+                                    Welcome to Sorting Visualizations!!
+                                        powered by: Matplot lib
+
+                                        MENU:
+
+                                        Press b for Bubble Sort
+                                        Press s for Selection Sort
+
+                 """
+    method = input(method_msg)
+    num = int(input("Enter number of integers: "))
+    items = [x + 1 for x in range(num)]
+    random.seed(time.time())
+    random.shuffle(items)
+    sort = Sort()
+
+    fig, ax = plt.subplots()
+
+    if method == "b":
+        title = "Bubble Sort"
+        bar_rects = ax.bar(range(len(items)), items, align="edge", color="red", edgecolor="blue")
+        generator = sort.bubble_sort(items)
+
+    
+        ax.set_xlim(0, num)
+        ax.set_ylim(0, int(1.07 * num))
+
+        # Place a text label in the upper-left corner of the plot to display
+        # number of operations performed by the sorting algorithm (each "yield"
+        # is treated as 1 operation).
+        text = ax.text(0.02, 0.95, "", transform=ax.transAxes)
+        iteration = [0]
+        def update_fig(items, rects, iteration):
+            for rect, val in zip(rects, items):
+                rect.set_height(val)
+            iteration[0] += 1
+            text.set_text("# of operations: {}".format(iteration[0]))
+
+        anim = animation.FuncAnimation(fig, func=update_fig,
+            fargs=(bar_rects, iteration), frames=generator, interval=1,
+            repeat=False)
+        plt.show()
+
+
+
+
+    if method == "s":
+        title = "Selection Sort"
+        bar_rects = ax.bar(range(len(items)), items, align="edge", color="blue", edgecolor="red")
+        generator = sort.selection_sort(items)
+
+  
+        ax.set_xlim(0, num)
+        ax.set_ylim(0, int(1.07 * num))
+
+        # Place a text label in the upper-left corner of the plot to display
+        # number of operations performed by the sorting algorithm (each "yield"
+        # is treated as 1 operation).
+        text = ax.text(0.02, 0.95, "", transform=ax.transAxes)
+        iteration = [0]
+        def update_fig(items, rects, iteration):
+            for rect, val in zip(rects, items):
+                rect.set_height(val)
+            iteration[0] += 1
+            text.set_text("# of operations: {}".format(iteration[0]))
+
+        anim = animation.FuncAnimation(fig, func=update_fig,
+            fargs=(bar_rects, iteration), frames=generator, interval=1,
+            repeat=False)
+        plt.show()
+
+
+
+    else:
+        sys.exit()
+        
+
---
+++ Python/Sort/SortingVisualizations/sorts.py
@@ -0,0 +1,44 @@
+
+class Sort:
+    def __init__(self):
+        self.temp = 0
+        self.swapped  = True
+
+    def swap(self, i, j, items):
+        print(items)
+        if i != j:
+            items[i], items[j] = items[j], items[i]
+
+    def bubble_sort(self, items):
+        if items == 1:
+            return
+
+        self.swapped = True
+        for i in range(len(items)-1):
+            if not self.swapped:
+                break
+            self.swapped = False
+            for j in range(len(items) - i - 1):
+                if items[j] > items[j+1]:
+                    self.swap(j, j+1, items)
+                    self.swapped = True
+                yield items
+
+
+    def selection_sort(self, items):
+        if len(items) == 1:
+            return
+
+        for i in range(len(items)):
+            min_val = items[i]
+            min_idx = i
+            for j in range(i, len(items)):
+                if items[j] < min_val:
+                    min_val = items[j]
+                    min_idx = j
+                   
+                yield items
+            self.swap(i, min_idx, items)
+            yield items
+
+
