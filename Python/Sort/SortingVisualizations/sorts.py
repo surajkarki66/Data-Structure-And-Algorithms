770a364dd890b5bf6251d2d3e6efca563d0d2002
surajkarki66 <suraj.karki500@gmail.com>
2020-02-23 21:44:42 +0545

    Finished mergesort

--- Python/Sort/SortingVisualizations/sorts.py
+++ Python/Sort/SortingVisualizations/sorts.py
@@ -52,7 +52,47 @@ class Sort:
                 print(items)
                 yield items
 
-          
+
+
+    def mergesort(self,A, start, end):
+        """Merge sort."""
+
+        if end <= start:
+            return
+
+        mid = start + ((end - start + 1) // 2) - 1
+        yield from self.mergesort(A, start, mid)
+        yield from self.mergesort(A, mid + 1, end)
+        yield from self.merge(A, start, mid, end)
+        yield A
+
+    def merge(self, A, start, mid, end):
+        """Helper function for merge sort."""
+        
+        merged = []
+        leftIdx = start
+        rightIdx = mid + 1
+
+        while leftIdx <= mid and rightIdx <= end:
+            if A[leftIdx] < A[rightIdx]:
+                merged.append(A[leftIdx])
+                leftIdx += 1
+            else:
+                merged.append(A[rightIdx])
+                rightIdx += 1
+
+        while leftIdx <= mid:
+            merged.append(A[leftIdx])
+            leftIdx += 1
+
+        while rightIdx <= end:
+            merged.append(A[rightIdx])
+            rightIdx += 1
+
+        for i, sorted_val in enumerate(merged):
+            A[start + i] = sorted_val
+            yield A
+
 
 
 
