1324b328fc19716899a83c7d8e1e0404747649d1
surajkarki66 <suraj.karki500@gmail.com>
2020-02-26 16:48:53 +0545

    Finished quicksort

--- Python/Sort/SortingVisualizations/app.py
+++ Python/Sort/SortingVisualizations/app.py
@@ -20,6 +20,7 @@ if __name__ == "__main__":
                                         Press s for Selection Sort
                                         Press i for insertion Sort
                                         Press m for merge Sort
+                                        Press q for quick Sort
 
                  """
     method = input(method_msg)
@@ -115,6 +116,32 @@ if __name__ == "__main__":
         title = "Merge Sort"
         bar_rects = ax.bar(range(len(items)), items, align="edge", color="blue", edgecolor="green")
         generator = sort.mergesort(items,0,num-1)
+
+  
+        ax.set_xlim(0, num)
+        ax.set_ylim(0, int(1.07 * num))
+
+        # Place a text label in the upper-left corner of the plot to display
+        # number of operations performed by the sorting algorithm (each "yield"
+        # is treated as 1 operation).
+        text = ax.text(0.02, 0.95, "", transform=ax.transAxes)
+        iteration = [0]
+        def update_fig(items, rects, iteration):
+            for rect, val in zip(rects, items):
+                rect.set_height(val)
+            iteration[0] += 1
+            text.set_text("# of operations: {}".format(iteration[0]))
+
+        anim = animation.FuncAnimation(fig, func=update_fig,
+            fargs=(bar_rects, iteration), frames=generator, interval=1,
+            repeat=False)
+       
+        plt.show()
+
+    if method == "q":
+        title = "Quick Sort"
+        bar_rects = ax.bar(range(len(items)), items, align="edge", color="orange", edgecolor="black")
+        generator = sort.quicksort(items,0,num-1)
 
   
         ax.set_xlim(0, num)
--- Python/Sort/SortingVisualizations/sorts.py
+++ Python/Sort/SortingVisualizations/sorts.py
@@ -1,4 +1,4 @@
-from playsound import playsound
+
 class Sort:
     def __init__(self):
         self.temp = 0
@@ -92,6 +92,70 @@ class Sort:
         for i, sorted_val in enumerate(merged):
             A[start + i] = sorted_val
             yield A
+
+    def partition(self, items, start, end):
+        loc = left = start
+        right = end
+        flag = False
+        while flag != True:
+            while((items[loc] <= items[right]) and (loc !=right)):
+                right -= 1       
+            
+            if (loc == right):
+                flag = True
+               
+
+            elif (items[loc] > items[right]):
+                items[loc], items[right] = items[right], items[loc]
+                loc = right
+                
+            if (flag != 1):
+                while((items[loc] >= items[left] and (loc != left))):
+                    left += 1
+                    
+
+                if (left == loc):
+                    flag = True
+
+                elif (items[loc] < items[left]):
+                    items[loc], items[left] = items[left], items[loc]
+                    loc = left
+                    
+        return loc
+
+    def quicksort(self, items, start, end):
+        if (start < end):
+            loc = self.partition(items, start, end)
+            yield items
+            yield from self.quicksort(items, start, loc-1)
+            yield from self.quicksort(items, loc+1, end)
+            yield items
+
+
+    
+
+"""
+    def quicksort(self, items, start, end):
+        if start >= end:
+            return
+
+        pivot = items[end]
+        pivotIdx = start
+
+        for i in range(start, end):
+            if items[i] < pivot:
+                items[i], items[pivotIdx] = items[pivotIdx], items[i]
+                pivotIdx += 1
+            yield items
+        items[end], items[pivotIdx] = items[pivotIdx], items[end]
+        yield items
+
+        yield from self.quicksort(items, start, pivotIdx - 1)
+        yield from self.quicksort(items, pivotIdx + 1, end)
+"""
+
+
+            
 
 
 
